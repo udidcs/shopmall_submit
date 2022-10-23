b9a14eba6a8f0bc069b6a60a5e9730a5c79e66e4
uddics <udidcs@naver.com>
2022-10-23 12:54:09 +0900

    test

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -94,7 +94,7 @@ public class ProductController {
         if (session == null) {
             return "redirect:home";
         }
-        return "/productForm";
+        return "productForm";
     }
 
     @GetMapping("/editProduct")
